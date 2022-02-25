dea211a3c9461bf49d0fa900862da64058c502d6
HyeSooYoon <yhs1790@naver.com>
2022-02-25 11:07:41 +0900

    git재커밋

--- src/test/java/com/diary/em/RedisRepositoryTest.java
+++ src/test/java/com/diary/em/RedisRepositoryTest.java
@@ -1,32 +1,75 @@
 package com.diary.em;
 
+import java.util.Map;
+import java.util.Set;
+
 import com.diary.em.Entity.Person;
 import com.diary.em.Repository.PersonRedisRepository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
 public class RedisRepositoryTest {
  
     @Autowired
-    private PersonRedisRepository repo;
+    private RedisTemplate<String, String> redisTemplate;
 
     @Test
-    void test() {
-        Person person = new Person("Park", 20);
+    void testStrings() {
+        // given
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        String key = "stringKey";
 
-        // 저장
-        repo.save(person);
+        // when
+        valueOperations.set(key, "hello");
 
-        // `keyspace:id` 값을 가져옴
-        repo.findById(person.getId());
+        // then
+        String value = valueOperations.get(key);
+        value.equals("hello");
+    }
 
-        // Person Entity 의 @RedisHash 에 정의되어 있는 keyspace (people) 에 속한 키의 갯수를 구함
-        repo.count();
 
-        // 삭제
-        repo.delete(person);
+    @Test
+    void testSet() {
+        // given
+        SetOperations<String, String> setOperations = redisTemplate.opsForSet();
+        String key = "setKey";
+
+        // when
+        setOperations.add(key, "h", "e", "l", "l", "o");
+
+        // then
+        Set<String> members = setOperations.members(key);
+        Long size = setOperations.size(key);
+
+        // assertThat(members).containsOnly("h", "e", "l", "o");
+        // assertThat(size).isEqualTo(4);
+    }
+
+    @Test
+    void testHash() {
+        // given
+        HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
+        String key = "hashKey";
+
+        // when
+        hashOperations.put(key, "hello", "world");
+
+        // then
+        Object value = hashOperations.get(key, "hello");
+        // assertThat(value).isEqualTo("world");
+
+        Map<Object, Object> entries = hashOperations.entries(key);
+        // assertThat(entries.keySet()).containsExactly("hello");
+        // assertThat(entries.values()).containsExactly("world");
+
+        Long size = hashOperations.size(key);
+        // assertThat(size).isEqualTo(entries.size());
     }
 }
